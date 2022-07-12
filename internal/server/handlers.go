c63ca5f0645ae9f8314534821ee9885377e56c93
i.danilov <iddanilov94@gmail.com>
2022-07-12 18:44:16 +0300

    [increment3] Add new handler get metrics

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/metricsAndAlerting/internal/handlers"
@@ -34,10 +35,49 @@ func NewHandler(storage *Storage) handlers.Handler {
 
 func (h *handler) Register(router *httprouter.Router) {
 	router.HandlerFunc(http.MethodPost, update, middleware.Middleware(h.UpdateMetrics))
+	router.HandlerFunc(http.MethodGet, value, middleware.Middleware(h.GetMetricByName))
+	router.HandlerFunc(http.MethodGet, metricsName, middleware.Middleware(h.GetMetricsName))
+}
+
+func (h *handler) GetMetricByName(w http.ResponseWriter, r *http.Request) error {
+	log.Println("Get Metrics", r.URL)
+	var response []byte
+	urlValue := strings.Split(r.URL.Path, "/")
+	if len(urlValue) < 5 {
+		w.WriteHeader(404)
+		return middleware.ErrNotFound
+	}
+
+	if strings.ToLower(urlValue[2]) == "gauge" {
+		var mapStorage map[string]client.GaugeMetric
+		jsonMetrics, _ := json.Marshal(h.storage)
+		json.Unmarshal(jsonMetrics, &mapStorage)
+
+		response = []byte(fmt.Sprintf("%f", mapStorage[urlValue[3]].Value))
+	}
+
+	if strings.ToLower(urlValue[2]) == "counter" {
+		var mapStorage map[string]client.CountMetric
+		jsonMetrics, _ := json.Marshal(h.storage)
+		json.Unmarshal(jsonMetrics, &mapStorage)
+
+		response = []byte(fmt.Sprintf("%v", mapStorage[urlValue[3]].Value))
+	}
+
+	w.WriteHeader(200)
+	w.Write(response)
+
+	return nil
+}
+
+func (h *handler) GetMetricsName(w http.ResponseWriter, r *http.Request) error {
+	w.WriteHeader(200)
+	w.Write([]byte(CreateResponse()))
+	return nil
 }
 
 func (h *handler) UpdateMetrics(w http.ResponseWriter, r *http.Request) error {
-	log.Println("UpdateMetrics Get Metrics", r.URL)
+	log.Println("UpdateMetrics Metrics", r.URL)
 	urlValue := strings.Split(r.URL.Path, "/")
 	if len(urlValue) < 5 {
 		w.WriteHeader(404)
@@ -76,4 +116,42 @@ func (h *handler) UpdateMetrics(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	return nil
+}
+
+func CreateResponse() string {
+	baseHTML := `
+	<h1>
+		<ul>
+			<li>Alloc</li>
+			<li>BuckHashSys</li>
+			<li>Frees</li>
+			<li>GCCPUFraction</li>
+			<li>GCSys</li>
+			<li>HeapAlloc</li>
+			<li>HeapIdle</li>
+			<li>HeapInuse</li>
+			<li>HeapObjects</li>
+			<li>HeapReleased</li>
+			<li>HeapSys</li>
+			<li>LastGC</li>
+			<li>Lookups</li>
+			<li>MCacheInuse</li>
+			<li>MCacheSys</li>
+			<li>MSpanInuse</li>
+			<li>MSpanSys</li>
+			<li>Mallocs</li>
+			<li>NextGC</li>
+			<li>NumForcedGC</li>
+			<li>NumGC</li>
+			<li>OtherSys</li>
+			<li>PauseTotalNs</li>
+			<li>StackInuse</li>
+			<li>StackSys</li>
+			<li>Sys</li>
+			<li>TotalAlloc</li>
+			<li>PollCount</li>
+			<li>RandomValue</li>
+		</ul>
+	</h1>`
+	return baseHTML
 }
