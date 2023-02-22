7488c6b584249b1df686d341083dbd3f4a4ef563
Ilya Danilov <iddanilov94@gmail.com>
2023-02-22 14:41:50 +0300

    Fix agent config

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,9 +1,7 @@
 // Package agent/main running agent application
 package main
 
-import (
-	"github.com/iddanilov/metricsAndAlerting/cmd/agent/app"
-)
+import "github.com/iddanilov/metricsAndAlerting/cmd/agent/app"
 
 func main() {
 	app.Run()
